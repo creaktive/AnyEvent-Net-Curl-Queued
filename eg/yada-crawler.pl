bdf3484835e318d8f839e8c1bc3ecb9e1b3839ab
Stanislaw Pusep <creaktive@gmail.com>
2000-03-20 18:00:00 -0300

    eg/yada-crawler.pl: simplified parser initialization

--- eg/yada-crawler.pl
+++ eg/yada-crawler.pl
@@ -6,20 +6,18 @@ use utf8::all;
 use Web::Scraper::LibXML;
 use YADA;
 
-my $parser = scraper {
-    process q(html title), title => q(text);
-    process q(a), q(links[]) => q(@href);
-};
-
 YADA->new(common_opts => { encoding => '' }, http_response => 1, max => 4)
 ->append([qw[http://localhost/manual/]] => sub {
     my ($self) = @_;
     my $response = $self->response;
     if ($response->is_success and $response->content_is_html) {
-        my $parsed = $parser->scrape($response->decoded_content, $self->final_url);
-        printf qq(%-64s %s\n), $self->final_url, $parsed->{title} =~ s/\r?\n/ /rsx;
+        my $doc = scraper {
+            process q(html title), title => q(text);
+            process q(a), q(links[]) => q(@href);
+        }->scrape($response->decoded_content, $self->final_url);
+        printf qq(%-64s %s\n), $self->final_url, $doc->{title} =~ s/\r?\n/ /rsx;
         $self->queue->prepend([
-            grep { $_->can(q(host)) and $_->host eq $self->initial_url->host } @{$parsed->{links}}
-        ] => __SUB__) if q(ARRAY) eq ref $parsed->{links};
+            grep { $_->can(q(host)) and $_->host eq $self->initial_url->host } @{$doc->{links}}
+        ] => __SUB__) if q(ARRAY) eq ref $doc->{links};
     }
 })->wait;
