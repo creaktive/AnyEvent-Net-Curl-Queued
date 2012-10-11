b5bdf7a8189ea2ef41050820809ecd97fedd0ba1
Stanislaw Pusep <creaktive@gmail.com>
2012-10-11 19:31:11 -0300

    YADA DWIM: documented & tested

--- lib/YADA.pm
+++ lib/YADA.pm
@@ -29,6 +29,93 @@ Use L<AnyEvent::Net::Curl::Queued> with fewer keystrokes.
 Also, the I<easy things should be easy> side of the package.
 For the I<hard things should be possible> side, refer to the complete L<AnyEvent::Net::Curl::Queued> documentation.
 
+=head1 USAGE
+
+The example in L</SYNOPSIS> is equivalent to:
+
+    #!/usr/bin/env perl
+    use common::sense;
+
+    use AnyEvent::Net::Curl::Queued;
+    use AnyEvent::Net::Curl::Queued::Easy;
+
+    my $q = AnyEvent::Net::Curl::Queued->new;
+    $q->append(sub {
+        AnyEvent::Net::Curl::Queued::Easy->new({
+            initial_url => $_,
+            on_finish   => sub {
+                say $_[0]->final_url;
+                say ${$_[0]->header};
+            },
+        })
+    }) for qw(
+        http://www.cpan.org/modules/by-category/02_Language_Extensions/
+        http://www.cpan.org/modules/by-category/02_Perl_Core_Modules/
+        http://www.cpan.org/modules/by-category/03_Development_Support/
+        ...
+        http://www.cpan.org/modules/by-category/27_Pragma/
+        http://www.cpan.org/modules/by-category/28_Perl6/
+        http://www.cpan.org/modules/by-category/99_Not_In_Modulelist/
+    );
+    $q->wait;
+
+As you see, L<YADA> overloads C<append>/C<prepend> from L<AnyEvent::Net::Curl::Queued>, adding implicit constructor for the worker object.
+It also makes both methods return a reference to the queue object, so (almost) everything gets chainable.
+The implicit constructor is triggered only when C<append>/C<prepend> receives multiple arguments.
+The order of arguments (mostly) doesn't matter.
+Their meaning is induced by their reference type:
+
+=for :list
+* String (non-reference) or L<URI>: assumed as L<AnyEvent::Net::Curl::Queued::Easy/initial_url> attribute. Passing several URLs will construct & enqueue several workers;
+* Array: process a batch of URLs;
+* Hash: attributes set for each L<AnyEvent::Net::Curl::Queued::Easy> instantiated. Passing several hashes will merge them, overwriting values for duplicate keys;
+* C<sub { ... }>: assumed as L<AnyEvent::Net::Curl::Queued::Easy/on_finish> attribute;
+* C<sub { ... }, sub { ... }>: the first block is assumed as L<AnyEvent::Net::Curl::Queued::Easy/on_init> attribute, while the second one is assumed as L<AnyEvent::Net::Curl::Queued::Easy/on_finish>.
+
+=head2 Beware!
+
+L<YADA> tries to follow the I<principle of least astonishment>, at least when you play nicely.
+All the following snippets have the same meaning:
+
+    $q->append(
+        { retry => 3 },
+        'http://www.cpan.org',
+        'http://metacpan.org',
+        sub { $_[0]->setopt(verbose => 1) }, # on_init placeholder
+        \&on_finish,
+    );
+
+    $q->append(
+        [qw[
+            http://www.cpan.org
+            http://metacpan.org
+        ]],
+        { retry => 3, opts => { verbose => 1 } },
+        \&on_finish,
+    );
+
+    $q->append(
+        URI->new($_) => \&on_finish,
+        { retry => 3, opts => { verbose => 1 } },
+    ) for qw[
+        http://www.cpan.org
+        http://metacpan.org
+    ];
+
+    $q->append(
+        [qw[
+            http://www.cpan.org
+            http://metacpan.org
+        ]] => {
+            retry       => 3,
+            opts        => { verbose => 1 },
+            on_finish   => \&on_finish,
+        }
+    );
+
+However, B<you will be astonished> if you specify multiple distinct C<on_init> and C<on_finish> or try to sneak in C<initial_url> through attributes!
+At least, RTFC if you seriously attempt to do that.
+
 =cut
 
 use strict;
--- t/44-yada-simple.t
+++ t/44-yada-simple.t
@@ -12,7 +12,7 @@ use YADA;
 
 my $server = Test::HTTP::AnyEvent::Server->new;
 
-my $q = YADA->new;
+my $q = YADA->new({ allow_dups => 1 });
 for my $i (1 .. 10) {
     for my $method (qw(append prepend)) {
         $q->$method(
@@ -24,6 +24,42 @@ for my $i (1 .. 10) {
         );
     }
 }
+
+my @urls = ($server->uri . 'echo/head') x 2;
+$urls[-1] =~ s{\b127\.0\.0\.1\b}{localhost};
+my @opts = (referer => 'http://www.cpan.org/');
+my $on_finish = sub {
+    my ($self, $r) = @_;
+    isa_ok($self->res, qw(HTTP::Response));
+    like($self->res->decoded_content, qr{\bReferer\s*:\s*\Q$opts[1]\E}isx);
+};
+
+$q->append(
+    { http_response => 1 },
+    @urls,
+    sub { $_[0]->setopt(@opts) }, # on_init placeholder
+    $on_finish,
+);
+
+$q->append(
+    [ @urls ],
+    { http_response => 1, opts => { @opts } },
+    $on_finish,
+);
+
+$q->append(
+    URI->new($_) => $on_finish,
+    { http_response => 1, opts => { @opts } },
+) for @urls;
+
+$q->append(
+    \@urls => {
+        http_response   => 1,
+        opts            => { @opts },
+        on_finish       => $on_finish,
+    }
+);
+
 $q->wait;
 
-done_testing(20);
+done_testing(20 + 4 * (scalar @urls) * 2);
