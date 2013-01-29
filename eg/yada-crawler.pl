6a6c3a1bca4c2e1ac13a63276ed30e39feb2ac30
Stanislaw Pusep <creaktive@gmail.com>
2013-01-29 00:36:38 -0200

    eg/yada-crawler.pl cleanup

--- eg/yada-crawler.pl
+++ eg/yada-crawler.pl
@@ -9,15 +9,15 @@ use YADA;
 YADA->new(common_opts => { encoding => '' }, http_response => 1, max => 4)
 ->append([qw[http://localhost/manual/]] => sub {
     my ($self) = @_;
-    my $response = $self->response;
-    if ($response->is_success and $response->content_is_html) {
+    if ($self->response->is_success and $self->response->content_is_html) {
         my $doc = scraper {
             process q(html title), title => q(text);
             process q(a), q(links[]) => q(@href);
-        }->scrape($response->decoded_content, $self->final_url);
+        }->scrape($self->response->decoded_content => $self->final_url);
         printf qq(%-64s %s\n), $self->final_url, $doc->{title} =~ s/\r?\n/ /rsx;
         $self->queue->prepend([
-            grep { $_->can(q(host)) and $_->host eq $self->initial_url->host } @{$doc->{links}}
-        ] => __SUB__) if q(ARRAY) eq ref $doc->{links};
+            grep { $_->can(q(host)) and $_->host eq $self->initial_url->host }
+            @{$doc->{links} // []}
+        ] => __SUB__);
     }
 })->wait;
